e5ce01a5dd64398c0a456ce2071d2e123e718c7f
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2014-05-22 07:00:00 -0400

    display all the images

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,1 +1,17 @@
-console.log('coffeescript working');
+var displayImages;
+
+displayImages = function(data) {
+  var html, list;
+  list = $('#image-list');
+  html = '';
+  $.each(data, function(i) {
+    return html += "<li><img src='" + data[i].image_url + "' /></li>";
+  });
+  return list.append(html);
+};
+
+$.ajax({
+  url: 'api/images',
+  success: displayImages,
+  dataType: 'json'
+});
--- views/javascripts/scripts.coffee
+++ views/javascripts/scripts.coffee
@@ -1,1 +1,11 @@
-console.log 'coffeescript working'
+displayImages = (data) ->
+  list = $('#image-list')
+  html = ''
+  $.each data, (i) ->
+    html += "<li><img src='#{data[i].image_url}' /></li>"
+  list.append(html)
+
+$.ajax
+  url: 'api/images'
+  success: displayImages
+  dataType: 'json'
