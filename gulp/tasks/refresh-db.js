8e5c41667690baecf55649b31bcbf7e1fbefe1bd
Morgan Wigmanich <mwigmanich@patientslikeme.com>
2017-07-31 07:00:00 -0400

    fix lint errors

--- gulp/tasks/refresh-db.js
+++ gulp/tasks/refresh-db.js
@@ -9,10 +9,10 @@ const parseMongoUrl = require('../helpers/parseMongoUrl');
 // download production db and import to localdb (logout first)
 gulp.task('refresh-db', () => {
   const devDb = parseMongoUrl(process.env.MONGODB_DEV_URL);
-  const prodDb = parseMongoUrl(process.env.MONGODB_PROD_URL || process.env.MONGOHQ_URL);
+  const { host, port, database, username, password } = parseMongoUrl(process.env.MONGODB_PROD_URL || process.env.MONGOHQ_URL);
   const dateStamp = moment().format('YYYYMMDD-hhmmss');
   const dumpDir = `${config.root}/dump/${dateStamp}`;
-  const mongoDumpCmd = `mongodump --host ${prodDb.host}:${prodDb.port} --db ${prodDb.database} -u ${prodDb.username} -p${prodDb.password} -o ${dumpDir}`;
+  const mongoDumpCmd = `mongodump --host ${host}:${port} --db ${database} -u ${username} -p${password} -o ${dumpDir}`;
   const mongoRestoreCmd = `mongorestore --drop -d ${devDb.database} ${dumpDir}/${devDb.database}`;
   return mkdirp(dumpDir, (err) => {
     if (err) { throw err; }
